74f8590675ea5e936db7016bbefe8214ed99c053
JAYAPRASAD B <127640948+Jayaprasad78@users.noreply.github.com>
2023-12-11 06:15:23 +0530

    Update mongodb.js

--- database/config/mongodb.js
+++ database/config/mongodb.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-//const url = 'mongodb+srv://soumyasri2245:asMJVVo05p6CKF3f@cluster0.u2ywt3o.mongodb.net/?retryWrites=true&w=majority'
+
 const url ='mongodb+srv://jayaprasadb718:asMJVVo05p6CKF3f@cluster0.cwwzkpt.mongodb.net/?retryWrites=true&w=majority'
 
 async function getEmpData() {
